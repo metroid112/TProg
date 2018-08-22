16a37499bf25710ed63f7e695c257ad9554a009e
Federico Nicolas Aguilera Tourn <federico.aguilera@fing.edu.uy>
2018-08-22 14:11:57 -0300

    Dividir tests de manejadores

--- TProg_Logica/src/test/ClassTest.java
+++ TProg_Logica/src/test/ClassTest.java
@@ -9,22 +9,32 @@ import manejadores.ManejadorListas;
 import manejadores.ManejadorUsuarios;
 
 public class ClassTest {
-
+	
 	public void testManejadores() {
+		testManejadorCategoria();
+		testManejadorLista();
+		testManejadorUsuario();
+	}
+
+	public void testManejadorCategoria() {
 		System.out.println("Testeo manejador categoria");
 		ManejadorCategorias manejadorCategoria = new ManejadorCategorias();
 		Categoria categoria = new Categoria();
 		manejadorCategoria.add(categoria);
 		Assert.assertEquals(true, manejadorCategoria.isMember(categoria));
 		System.out.println("Termino test manejador categoria\n");
-		
+	}
+	
+	public void testManejadorLista() {
 		System.out.println("Testeo manejador lista");
 		ManejadorListas manejadorLista = new ManejadorListas();
 		String listaDefecto = "Lista";
 		manejadorLista.add(listaDefecto);
 		Assert.assertEquals(true, manejadorLista.isMember(listaDefecto));
 		System.out.println("Termino test manejador lista\n");
-		
+	}
+	
+	public void testManejadorUsuario() {		
 		System.out.println("Testeo manejador usuario");
 		ManejadorUsuarios manejadorUsuario = new ManejadorUsuarios();
 		Usuario usuario = new Usuario();
