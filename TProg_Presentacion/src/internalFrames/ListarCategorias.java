18bffad81d454c4886b1660959b02a8a6aa0abca
Francisco Crocamo <frcrocamo@gmail.com>
2018-08-22 14:14:28 -0300

    Cambio de comboBox a JList

--- TProg_Presentacion/src/internalFrames/ListarCategorias.java
+++ TProg_Presentacion/src/internalFrames/ListarCategorias.java
@@ -11,6 +11,8 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
 
 @SuppressWarnings("serial")
 public class ListarCategorias extends JInternalFrame {
@@ -18,8 +20,6 @@ public class ListarCategorias extends JInternalFrame {
 	public ListarCategorias() {
 		setTitle("Lista de Categorias");
 		setBounds(0, 0, 640, 480);
-		
-		JComboBox comboBox = new JComboBox();
 		
 		JLabel lblCategoriasRegistradas = new JLabel("Categorias registradas:");
 		lblCategoriasRegistradas.setFont(new Font("Tahoma", Font.PLAIN, 13));
@@ -30,15 +30,26 @@ public class ListarCategorias extends JInternalFrame {
 				setVisible(false);
 			}
 		});
+		
+		JList<Object> list = new JList<Object>();
+		list.setModel(new AbstractListModel<Object>() {
+			String[] values = new String[] {"PRUEBA 1", "PRUEBA 2", "PRUEBA 3"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(119)
 					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(comboBox, Alignment.LEADING, 0, 173, Short.MAX_VALUE)
-						.addComponent(btnCerrar, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 173, Short.MAX_VALUE)
-						.addComponent(lblCategoriasRegistradas, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 173, Short.MAX_VALUE))
+						.addComponent(list, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+						.addComponent(btnCerrar, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+						.addComponent(lblCategoriasRegistradas, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE))
 					.addGap(117))
 		);
 		groupLayout.setVerticalGroup(
@@ -46,11 +57,11 @@ public class ListarCategorias extends JInternalFrame {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(50)
 					.addComponent(lblCategoriasRegistradas)
-					.addGap(37)
-					.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(225)
+					.addGap(32)
+					.addComponent(list)
+					.addGap(250)
 					.addComponent(btnCerrar)
-					.addContainerGap(34, Short.MAX_VALUE))
+					.addContainerGap(79, Short.MAX_VALUE))
 		);
 		getContentPane().setLayout(groupLayout);
 
