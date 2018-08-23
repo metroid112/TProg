de8c2ae44c88993fdb3994584adcc8167a95320d
JavierGianneo <javier.gianneo@gmail.com>
2018-08-23 20:04:58 -0300

    parte de la logica pensada

--- TProg_Logica/src/clases/ListaDefecto.java
+++ TProg_Logica/src/clases/ListaDefecto.java
@@ -1,4 +1,10 @@
 package clases;
 
 public class ListaDefecto extends Lista {
+	
+	private String nombre;
+	
+	public ListaDefecto(String nom){
+		this.nombre = nom;
+	}
 }
--- TProg_Logica/src/controladores/CtrlListas.java
+++ TProg_Logica/src/controladores/CtrlListas.java
@@ -2,8 +2,22 @@ package controladores;
 
 import interfaces.IListas;
 import manejadores.ManejadorListas;
+import clases.Lista;
+import clases.ListaDefecto;
 
 public class CtrlListas implements IListas {
 
 	private ManejadorListas manejadorListas = ManejadorListas.getManejadorListas();
+	private CtrlUsuariosCanales controladorUsuCan = new CtrlUsuariosCanales();
+	
+	public void ingresarListaParticular(String nombre, String usuario, boolean visibilidad){
+		//controladorUsuCan.ingresarListaParticular(nombre, usuario, visibilidad);
+	}
+	
+	public void ingresarListaDefecto(String nombre){
+		
+		//lo crea el manejador
+		//se notifica a todos los usuarios
+		//se crea en cada usuario
+	}
 }
