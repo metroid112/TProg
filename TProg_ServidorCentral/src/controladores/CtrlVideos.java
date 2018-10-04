a0c1c94522e6a5fabe6fef7052c831faf77253a0
Federico Nicolas Aguilera Tourn <federico.aguilera@fing.edu.uy>
2018-10-03 23:00:05 -0300

    funciones de interfaz

--- TProg_ServidorCentral/src/controladores/CtrlVideos.java
+++ TProg_ServidorCentral/src/controladores/CtrlVideos.java
@@ -2,6 +2,9 @@ package controladores;
 
 import java.time.Duration;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import clases.Categoria;
 import clases.Usuario;
@@ -13,6 +16,7 @@ import excepciones.NotFoundException;
 import interfaces.IVideos;
 import manejadores.ManejadorCategorias;
 import manejadores.ManejadorUsuarios;
+import manejadores.ManejadorVideos;
 
 public class CtrlVideos implements IVideos {
   private ManejadorCategorias manejadorCategoria = ManejadorCategorias.getManejadorCategorias();
@@ -37,12 +41,13 @@ public class CtrlVideos implements IVideos {
     Video video = user.getCanal().altaVideo(nombre, descripcion, duracion, url, categoria,
         fecha, visibilidad);
     categoria.addVideo(video);
+    ManejadorVideos.getManejadorVideos().add(video);
   }
 
   @Override
-  public DtVideo getDtVideo(String video, String usuario) {
-    Video vid = manejadorUsuario.get(usuario).getCanal().getVideoCanal(video);
-    return vid.getDt();
+  public DtVideo getDtVideo(int id) throws NotFoundException {
+    Video video = ManejadorVideos.getManejadorVideos().getById(id);
+    return video.getDt();
   }
 
   @Override
@@ -79,5 +84,14 @@ public class CtrlVideos implements IVideos {
     }
     vid.modificarDatos(nombre, descripcion, url, categoria, duracion, visible, fecha);
 
+  }
+
+  @Override
+  public Map<Integer, DtVideo> getDtVideos() {
+    Map<Integer, DtVideo> dtVideos = new HashMap<Integer, DtVideo>();
+    for(Entry<Integer, Video> video : ManejadorVideos.getManejadorVideos().getVideos().entrySet()) {
+      dtVideos.put(video.getKey(), video.getValue().getDt());
+    }
+    return dtVideos;
   }
 }
