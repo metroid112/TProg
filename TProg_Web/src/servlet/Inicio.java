dec2b62b9da954b41bd7c2648b30a21262e53239
Pato <federico.aguilera112@gmail.com>
2018-10-11 12:41:16 -0300

    clean

--- TProg_Web/src/servlet/Inicio.java
+++ TProg_Web/src/servlet/Inicio.java
@@ -10,23 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import utils.EstadoSesion;
 
-/**
- * Servlet implementation class Inicio
- */
 @WebServlet("/Inicio")
 public class Inicio extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
-  /**
-   * @see HttpServlet#HttpServlet()
-   */
   public Inicio() {
     super();
   }
 
-  /**
-   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-   */
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     iniciar(request, response);
@@ -37,15 +28,10 @@ public class Inicio extends HttpServlet {
     if (request.getSession().getAttribute("LOGIN") == null) {
       request.getSession().setAttribute("LOGIN", EstadoSesion.NO_LOGIN);
     }
-
   }
 
-  /**
-   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-   */
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     doGet(request, response);
   }
-
 }
