1058deaaaedbdfd26730741fc5c19f532ad81420
Francisco Crocamo <frcrocamo@gmail.com>
2018-10-14 09:32:26 -0300

    Test usuarios carga

--- TProg_ServidorCentral/src/test/CargaDatosTest.java
+++ TProg_ServidorCentral/src/test/CargaDatosTest.java
@@ -5,21 +5,38 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.text.ParseException;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import interfaces.Fabrica;
-import junit.framework.Assert;
+import interfaces.*;
 
 public class CargaDatosTest {
+  
+  public IUsuariosCanales iUsuarios = Fabrica.getIUsuariosCanales();
+  public IListas iListas = Fabrica.getIListas();
+  public IVideos iVideos = Fabrica.getIVideos();
+  public ICategorias iCategorias = Fabrica.getICategorias();
+  
 
-  @Test
-  public void test() {
+  @Before
+  public void carga() {
     try {
       Fabrica.getIDatos().cargaDatos();
     } catch (ParseException | IOException e) {
       e.printStackTrace();
     }
-    assertTrue(true);
+  }
+  
+  @Test
+  public void testUsuarios() {
+    System.out.println(iUsuarios.listarNombresUsuarios());
+    boolean fallo = false;
+    for (String nombre : iUsuarios.listarNombresUsuarios()) {
+      if (!iUsuarios.existeUsuario(nombre)) {
+        fallo = true;
+      }
+    }
+    assertFalse(fallo);
   }
 
 }
