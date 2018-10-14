2d03fbc27480be5f59f52fed9d552359cc546f6b
Francisco Crocamo <frcrocamo@gmail.com>
2018-10-14 11:26:41 -0300

    agrego otro test

---
+++ TProg_ServidorCentral/src/test/CtrlUsuariosCanalesTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CtrlUsuariosCanalesTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
