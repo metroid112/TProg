db0191a7c201b3c9036b99a0c055243d50310227
metroid112 <federico.aguilera112@gmail.com>
2018-10-17 14:17:21 -0300

    TODO

--- TProg_ServidorCentral/src/excepciones/NotFoundException.java
+++ TProg_ServidorCentral/src/excepciones/NotFoundException.java
@@ -3,6 +3,7 @@ package excepciones;
 @SuppressWarnings("serial")
 public class NotFoundException extends Exception {
 
+  // TODO: Cambiar notFound a object y agregar el lugar donde no se encuentra.
   public NotFoundException(String notFound) {
     super("No se ha encontrado " + notFound);
   }
