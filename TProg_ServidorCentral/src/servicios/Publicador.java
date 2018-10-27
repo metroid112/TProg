378a8459fa03d39998137188fa50aacc2e299ada
Francisco Crocamo <frcrocamo@gmail.com>
2018-10-27 15:20:34 -0300

    carga de datos pronta

--- TProg_ServidorCentral/src/servicios/Publicador.java
+++ TProg_ServidorCentral/src/servicios/Publicador.java
@@ -1,10 +1,15 @@
 package servicios;
+import java.io.IOException;
+import java.text.ParseException;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.ParameterStyle;
 import javax.jws.soap.SOAPBinding.Style;
 import javax.xml.ws.Endpoint;
+
+import interfaces.Fabrica;
 
 @WebService
 @SOAPBinding(style = Style.RPC, parameterStyle = ParameterStyle.WRAPPED)
@@ -31,5 +36,20 @@ public class Publicador {
   @WebMethod
   public String prueba2() {
     return "prueba 2";
+  }
+  @WebMethod
+  public void cargarDatos() {
+    try {
+      Fabrica.getIDatos().cargaDatos();
+    } catch (ParseException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
