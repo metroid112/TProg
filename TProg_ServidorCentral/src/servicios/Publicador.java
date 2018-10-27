179b161bbca2902e5015da6ff40ecec6c3e9bac4
Francisco Crocamo <frcrocamo@gmail.com>
2018-10-27 12:51:23 -0300

    servicio

--- TProg_ServidorCentral/src/servicios/Publicador.java
+++ TProg_ServidorCentral/src/servicios/Publicador.java
@@ -1,9 +1,29 @@
 package servicios;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.xml.ws.Endpoint;
 
+@WebService
+@SOAPBinding(style = Style.RPC, parameterStyle = ParameterStyle.WRAPPED)
 public class Publicador {
   
+  private Endpoint endpoint = null;
   public Publicador() {}
   
+  @WebMethod(exclude = true)
+  public void publicar(){
+       endpoint = Endpoint.publish("http://localhost:10135/publicador", this);
+  }
+  
+  @WebMethod(exclude = true)
+  public Endpoint getEndpoint() {
+          return endpoint;
+  }
+  
+  @WebMethod
   public String prueba(String texto) {
     return "Texto: " + texto;
   }
--- TProg_ServidorCentral/src/test/TestRunner.java
+++ TProg_ServidorCentral/src/test/TestRunner.java
@@ -4,14 +4,18 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+import servicios.Publicador;
+
 public class TestRunner {
   public static void main(String[] args) {
-    Result result = JUnitCore.runClasses(TestSuite.class);
-
-    for (Failure failure : result.getFailures()) {
-      System.out.println(failure.toString());
-    }
-
-    System.out.println(result.wasSuccessful());
+//    Result result = JUnitCore.runClasses(TestSuite.class);
+//
+//    for (Failure failure : result.getFailures()) {
+//      System.out.println(failure.toString());
+//    }
+//
+//    System.out.println(result.wasSuccessful());
+    Publicador publicador = new Publicador();
+    publicador.publicar();
   }
 }
