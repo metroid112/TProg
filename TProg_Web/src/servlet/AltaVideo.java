8e5b096e067c851e57b32e1b93220d23a33121d9
JavierGianneo <javier.gianneo@gmail.com>
2018-11-17 11:19:16 -0300

    lala

--- TProg_Web/src/servlet/AltaVideo.java
+++ TProg_Web/src/servlet/AltaVideo.java
@@ -75,7 +75,7 @@ public class AltaVideo extends HttpServlet {
               GregorianCalendar cal = new GregorianCalendar();
               cal.setTime(fecha);
               XMLGregorianCalendar fechaXML = DatatypeFactory.newInstance().newXMLGregorianCalendar(cal);
-              port.altaVideo(nick, nombre, descripcion, duracion.getSeconds(), url, "categoria", fechaXML,  //Categoria temporalmente hardcodeada
+              port.altaVideo(nick, nombre, descripcion, duracion.getSeconds(), url, categoria, fechaXML,
                   false);
             } catch (DatatypeConfigurationException e) {
               // TODO Auto-generated catch block
