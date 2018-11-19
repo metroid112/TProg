9b2e7af6d906b234d14ec4dc5ef476ca6daaafe9
Francisco Crocamo <frcrocamo@gmail.com>
2018-11-19 10:20:08 -0300

    No tests y sin publicar

--- TProg_ServidorCentral/src/test/TestRunner.java
+++ TProg_ServidorCentral/src/test/TestRunner.java
@@ -1,10 +1,8 @@
 package test;
 
-import servicios.Publicador;
-
 public class TestRunner {
   public static void main(String[] args) {
-    Publicador publicador = new Publicador();
-    publicador.publicar();
+//    Publicador publicador = new Publicador();
+//    publicador.publicar();
   }
 }
